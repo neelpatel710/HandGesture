e1e1d8c819c3b4c42b9aa435b33b13425dff8ecf
neelpatel710 <56323031+neelpatel710@users.noreply.github.com>
2020-02-27 23:32:07 -0500

    Add files via upload

---
+++ Hand Gesture/Hand Gesture/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hand_Gesture
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "HAND GESTURE RECOGNIZATION";
+            label1.Font = new Font(label1.Font.FontFamily, 20);
+            label1.ForeColor = Color.Crimson;
+            //label1.Margin = 30;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(@"Dynamic.py");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start(@"Static.py");
+        }
+    }
+}
